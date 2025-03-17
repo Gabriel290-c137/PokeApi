d5099b4e6405790f001f72a1afffddcb83abd8aa
Gabriel290-c137 <limbert.villca@ucb.edu.bo>
2025-03-16 21:19:49 -0400

    Descripción de los cambios

---
+++ src/app/tab1/tab1.page.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+import { IonHeader, IonToolbar, IonTitle, IonContent,IonItem, IonLabel, IonList } from '@ionic/angular/standalone';
+import { ExploreContainerComponent } from '../explore-container/explore-container.component';
+import { Pokemon } from '../interfaces/pokemon.interface';
+import { PokemonService } from '../services/pokemon.service';
+import { CommonModule } from '@angular/common';
+
+@Component({
+    selector: 'app-tab1',
+    templateUrl: 'tab1.page.html',
+    styleUrls: ['tab1.page.scss'],
+    standalone: true,
+    imports: [IonHeader, IonToolbar, IonTitle, IonContent, ExploreContainerComponent, IonItem, IonLabel, IonList, CommonModule],
+})
+export class Tab1Page implements OnInit {
+    pokemonList: Pokemon[] = [];
+
+    constructor(private pokemonService: PokemonService) {}
+
+    ngOnInit() {
+        this.loadPokemonList();
+    }
+
+    loadPokemonList() {
+        this.pokemonService.getPokemonList().subscribe((response) => {
+            this.pokemonList = response.results;
+        });
+    }
+}
